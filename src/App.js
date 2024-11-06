fb25719a1126b7c9460a19fc1be4d327adf5fd1d
Jacob Wilson <jcbwlsn04@gmail.com>
2024-11-06 10:06:31 -0600

    Fixed Validations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 // import Particles from 'react-particles-js';
 import ParticlesBg from "particles-bg";
-import Clarifai from "clarifai";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
 import Signin from "./components/Signin/Signin";
@@ -10,11 +9,6 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
-
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
-  apiKey: "9c1e283685084a86b9a71aaaa527cbe3",
-});
 
 // No Longer need this. Updated to particles-bg
 // const particlesOptions = {
@@ -59,7 +53,8 @@ class App extends Component {
   };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data[0].region_info.bounding_box;
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
@@ -82,10 +77,6 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-    // If that isn't working, then that means you will have to wait until their servers are back up.
     fetch("http://localhost:3000/clarifai", {
       method: "post",
       headers: { "Content-Type": "application/json" },
